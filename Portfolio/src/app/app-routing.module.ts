ac2471e289a68dc2a4a10e5369656b0ba456d38f
marttuga <martaaportugall@gmail.com>
2023-10-30 01:10:39 +0100

    apagar a pagina de about

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutPageComponent } from 'src/views/about-page/about-page.component';
 import { InicialPageComponent } from 'src/views/inicial-page/inicial-page.component';
 
 const routes: Routes = [
   { path: '',redirectTo: 'inicial-page', pathMatch: 'full' },
   { path: 'inicial-page', component: InicialPageComponent,},
-  { path: 'about-page', component: AboutPageComponent,},
 
 ];
 
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { HttpClientModule } from '@angular/common/http'; // Importe HttpClientMo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicialPageComponent } from 'src/views/inicial-page/inicial-page.component';
-import { AboutPageComponent } from '../views/about-page/about-page.component';
+import { ReactiveFormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
+import { FormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
 
 @NgModule({
   declarations: [
     AppComponent,
     InicialPageComponent,
-    AboutPageComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

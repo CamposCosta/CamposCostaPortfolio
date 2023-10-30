fa8c05ad0d5e96a0d8e589100bbc174eb0f037b2
marttuga <martaaportugall@gmail.com>
2023-10-30 02:12:41 +0100

    work component

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicialPageComponent } from 'src/views/inicial-page/inicial-page.component';
+import { WorkPageComponent } from 'src/views/work-page/work-page.component';
 
 const routes: Routes = [
   { path: '',redirectTo: 'inicial-page', pathMatch: 'full' },
   { path: 'inicial-page', component: InicialPageComponent,},
+  { path: 'work-page', component: WorkPageComponent,},
+
 
 ];
 
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicialPageComponent } from 'src/views/inicial-page/inicial-page.component';
 import { ReactiveFormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
-import { FormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
+import { FormsModule } from '@angular/forms';
+import { WorkPageComponent } from '../views/work-page/work-page.component';// Importe o ReactiveFormsModule
 
 @NgModule({
   declarations: [
     AppComponent,
     InicialPageComponent,
+    WorkPageComponent,
     
   ],
   imports: [

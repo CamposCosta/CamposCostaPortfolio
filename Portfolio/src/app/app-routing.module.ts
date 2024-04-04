43cb0bdba36109eb2e0b5cd174d2f86e7988f2a5
AndreFeMarques <1200605@isep.ipp.pt>
2024-04-04 23:48:35 +0100

    Update docs folder with new Angular build

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -6,17 +6,16 @@ import { AboutPageComponent } from './views/about-page/about-page.component';
 import { WorkPage3Component } from './views/work-page3/work-page3.component';
 
 const routes: Routes = [
-  { path: '',redirectTo: 'cover-page', pathMatch: 'full' },
-  { path: 'cover-page', component: CoverPageComponent,},
-  { path: 'inicial-page', component: InicialPageComponent,},
-  { path: 'work-page3', component: WorkPage3Component,},
-  { path: 'about-page', component: AboutPageComponent,},
-
-
+  { path: '', redirectTo: 'cover-page', pathMatch: 'full' },
+  { path: 'cover-page', component: CoverPageComponent },
+  { path: 'inicial-page', component: InicialPageComponent },
+  { path: 'work-page3', component: WorkPage3Component },
+  { path: 'about-page', component: AboutPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // Adicione useHash: true na configuração do RouterModule
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
